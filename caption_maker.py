14749a7ea436fd8cf3b5b0078bf8cfdd0eb5a572
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-07-23 13:11:50 +0530

    WE'RE MAKING OUR OWN CUSTOM DFXP WRITER LFGGGGGGGGG 🥳🥳🥳

---
+++ caption_maker.py
@@ -0,0 +1,9 @@
+from video_hex import video_to_rgb
+import numpy as np
+
+def caption_maker(rgb_frames):
+    caption = []
+    caption.append("<?xml version=\"1.0\" encoding=\"utf-8\" ?>")
+    caption.append("<transcript>")
+
+    caption.append("</transcript>")
