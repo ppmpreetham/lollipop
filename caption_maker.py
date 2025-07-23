1ae2f6969bb049f446d88db8e5cc41d8938eaf2a
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-07-23 13:38:42 +0530

    make file outputter

--- caption_maker.py
+++ caption_maker.py
@@ -28,13 +28,28 @@ def makeRow(start: float, dur: float, hex_list: list[str]) -> str:
 
 
 # hexes = [
-#     "464037", "433C33", "413B32", "403930", "41392F", "40382D",
+#     "464037", "464037", "413B32", "403930", "41392F", "40382D",
 #     "3E372E", "3A352F", "373430", "343330", "363737", "363838"
 # ]
 # print(makeRow(0.0, 0.083, hexes))
 
-def caption_maker(rgb_frames):
+def rgb_to_hex_list(rgb_frame: np.ndarray) -> list[str]:
+    h, w, _ = rgb_frame.shape
+    flattened = rgb_frame.reshape(h * w, 3)
+    return ['{:02X}{:02X}{:02X}'.format(r, g, b) for r, g, b in flattened]
+
+def caption_maker(rgb_frames: np.array, fps: float = 12.0) -> str:
     caption = []
     caption.append("<?xml version=\"1.0\" encoding=\"utf-8\" ?>")
     caption.append("<transcript>")
+    
+    frame_duration = 1 / fps
+
+    for i, frame in enumerate(rgb_frames):
+        start = i * frame_duration
+        hex_list = rgb_to_hex_list(frame)
+        caption.append(makeRow(start, frame_duration, hex_list))
+
+
     caption.append("</transcript>")
+    return ''.join(caption)
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from caption_maker import caption_maker
 from video_hex import width_blocks_finder, video_to_rgb
 
 def main():
+    FILE_NAME = "tests/BatMobile.mp4"
     print("Hello from lollipop!")
     print(width_blocks_finder(30, 30, 30))
-    print(video_to_rgb("tests/BatMobile.mp4", pixelHeight=30))
+    caption = caption_maker(video_to_rgb(FILE_NAME, pixelHeight=30))
+
+    NEW_FILE_NAME = FILE_NAME.rsplit('.', 1)[0] + "_caption.dfxp"
+    with open(NEW_FILE_NAME, 'w', encoding="utf-8") as f:
+        f.write(caption)
 
 
 if __name__ == "__main__":
